98ab185503f338b14adc724c0bf92c5c78089d64
Victor Lévesque <levesque.vic@gmail.com>
2017-06-14 11:24:35 -0400

    please work

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class MainPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SideMenu = new System.Windows.Forms.GroupBox();
+            this.QuitButton = new System.Windows.Forms.Button();
+            this.NewGameButton = new System.Windows.Forms.Button();
+            this.RebButton = new System.Windows.Forms.Button();
+            this.YellowButton = new System.Windows.Forms.Button();
+            this.GreenButton = new System.Windows.Forms.Button();
+            this.BlueButton = new System.Windows.Forms.Button();
+            this.SideMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SideMenu
+            // 
+            this.SideMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.SideMenu.Controls.Add(this.QuitButton);
+            this.SideMenu.Controls.Add(this.NewGameButton);
+            this.SideMenu.Location = new System.Drawing.Point(668, 12);
+            this.SideMenu.Name = "SideMenu";
+            this.SideMenu.Size = new System.Drawing.Size(273, 377);
+            this.SideMenu.TabIndex = 0;
+            this.SideMenu.TabStop = false;
+            this.SideMenu.Text = "Menu";
+            // 
+            // QuitButton
+            // 
+            this.QuitButton.Location = new System.Drawing.Point(6, 96);
+            this.QuitButton.Name = "QuitButton";
+            this.QuitButton.Size = new System.Drawing.Size(261, 65);
+            this.QuitButton.TabIndex = 6;
+            this.QuitButton.Text = "Quit";
+            this.QuitButton.UseVisualStyleBackColor = true;
+            this.QuitButton.Click += new System.EventHandler(this.QuitButton_Click);
+            // 
+            // NewGameButton
+            // 
+            this.NewGameButton.Location = new System.Drawing.Point(6, 25);
+            this.NewGameButton.Name = "NewGameButton";
+            this.NewGameButton.Size = new System.Drawing.Size(261, 65);
+            this.NewGameButton.TabIndex = 5;
+            this.NewGameButton.Text = "New Game";
+            this.NewGameButton.UseVisualStyleBackColor = true;
+            this.NewGameButton.Click += new System.EventHandler(this.NewGameButton_Click);
+            // 
+            // RebButton
+            // 
+            this.RebButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.RebButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RebButton.Location = new System.Drawing.Point(12, 12);
+            this.RebButton.Name = "RebButton";
+            this.RebButton.Size = new System.Drawing.Size(322, 188);
+            this.RebButton.TabIndex = 1;
+            this.RebButton.Text = "RED";
+            this.RebButton.UseVisualStyleBackColor = false;
+            this.RebButton.Click += new System.EventHandler(this.RebButton_Click);
+            // 
+            // YellowButton
+            // 
+            this.YellowButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.YellowButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.YellowButton.Location = new System.Drawing.Point(340, 12);
+            this.YellowButton.Name = "YellowButton";
+            this.YellowButton.Size = new System.Drawing.Size(322, 188);
+            this.YellowButton.TabIndex = 2;
+            this.YellowButton.Text = "YELLOW";
+            this.YellowButton.UseVisualStyleBackColor = false;
+            this.YellowButton.Click += new System.EventHandler(this.YellowButton_Click_1);
+            // 
+            // GreenButton
+            // 
+            this.GreenButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.GreenButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GreenButton.Location = new System.Drawing.Point(12, 206);
+            this.GreenButton.Name = "GreenButton";
+            this.GreenButton.Size = new System.Drawing.Size(322, 183);
+            this.GreenButton.TabIndex = 3;
+            this.GreenButton.Text = "GREEN";
+            this.GreenButton.UseVisualStyleBackColor = false;
+            this.GreenButton.Click += new System.EventHandler(this.GreenButton_Click_1);
+            // 
+            // BlueButton
+            // 
+            this.BlueButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.BlueButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BlueButton.Location = new System.Drawing.Point(340, 206);
+            this.BlueButton.Name = "BlueButton";
+            this.BlueButton.Size = new System.Drawing.Size(322, 183);
+            this.BlueButton.TabIndex = 4;
+            this.BlueButton.Text = "BLUE";
+            this.BlueButton.UseVisualStyleBackColor = false;
+            this.BlueButton.Click += new System.EventHandler(this.BlueButton_Click);
+            // 
+            // MainPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(953, 401);
+            this.Controls.Add(this.BlueButton);
+            this.Controls.Add(this.GreenButton);
+            this.Controls.Add(this.YellowButton);
+            this.Controls.Add(this.RebButton);
+            this.Controls.Add(this.SideMenu);
+            this.Name = "MainPage";
+            this.Text = "SimonSays";
+            this.SideMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox SideMenu;
+        private System.Windows.Forms.Button QuitButton;
+        private System.Windows.Forms.Button NewGameButton;
+        private System.Windows.Forms.Button RebButton;
+        private System.Windows.Forms.Button YellowButton;
+        private System.Windows.Forms.Button GreenButton;
+        private System.Windows.Forms.Button BlueButton;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class MainPage : Form
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        // Variables Declarations
+        bool verify_sequ = false;
+        int k, h,round, j;
+        int[] Table = new int[75];
+        double FlashTime = 0.5;
+
+
+
+        private void RebButton_Click(object sender, EventArgs e)
+        {
+            // Flash For user feedback
+            RebButton.BackColor = Color.FromArgb(255, 0, 0);
+            this.Refresh();
+            WaitSecond(0.1);
+            RebButton.BackColor = Color.FromArgb(255, 128, 128); // Light red
+            this.Refresh();
+            WaitSecond(0.1);
+
+            VerifySequence(1);
+
+        }
+
+        private void YellowButton_Click_1(object sender, EventArgs e)
+        {
+            // Flash For user feedback
+            YellowButton.BackColor = Color.FromArgb(255, 255, 0);
+            this.Refresh();
+            WaitSecond(0.1);
+            YellowButton.BackColor = Color.FromArgb(255, 255, 128); // Light yellow
+            this.Refresh();
+            WaitSecond(0.1);
+
+            VerifySequence(2);
+        }
+
+        private void GreenButton_Click_1(object sender, EventArgs e)
+        {
+            // Flash For user feedback
+            GreenButton.BackColor = Color.FromArgb(0, 255, 0);
+            this.Refresh();
+            WaitSecond(0.1);
+            GreenButton.BackColor = Color.FromArgb(128, 255, 128); // Light green
+            this.Refresh();
+            WaitSecond(0.1);
+
+            VerifySequence(3);
+
+        }
+
+
+        private void BlueButton_Click(object sender, EventArgs e)
+        {
+            // Flash For user feedback
+            BlueButton.BackColor = Color.FromArgb(0, 0, 255);
+            this.Refresh();
+            WaitSecond(0.1);
+            BlueButton.BackColor = Color.FromArgb(128, 128, 255); // Light blue
+            this.Refresh();
+            WaitSecond(0.1);
+
+            VerifySequence(4);
+        }
+
+        private void QuitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void NewGameButton_Click(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+        /*
+        * ----------------------------------------------------------------------------
+        * Name Fuction : WaitSecond
+        * Return       : 
+        * Description  : counts for X seconds
+        * ----------------------------------------------------------------------------
+        */
+
+        private void WaitSecond(double x)
+        {
+            Stopwatch s = new Stopwatch();
+            s.Start();
+            while (s.Elapsed < TimeSpan.FromSeconds(x))
+            {
+                //Wait
+            }
+            s.Stop();
+        }
+
+
+        /*
+         * ----------------------------------------------------------------------------
+         * Name Fuction : StartGame
+         * Return       : 
+         * Description  : Start the game at round 0 
+         * ----------------------------------------------------------------------------
+        */
+        private void StartGame()
+        {
+            //Make a table of 75 int table and fill them with radom int from 1 to 4
+
+            // Need to generate new radom number for patern
+            Random rnd = new Random();
+            for (h = 0; h <= 74; h++) {
+                Table[h] = rnd.Next(1, 5);
+            }
+
+            // Set Round
+            round= 0;
+
+            //321...go
+            MessageBox.Show("3,2,1...go!");
+
+            //Show first round
+            verify_sequ = false;
+            DisplaySequence();
+            verify_sequ = true;
+        }
+
+        /*
+         * ----------------------------------------------------------------------------
+         * Name Fuction : VerifySequence
+         * Return       : 
+         * Description  : When a button is pressed verify if the color is correct else loose the game
+         * ----------------------------------------------------------------------------
+        */
+
+        private void VerifySequence(int color_ID)
+        {
+            if (verify_sequ)
+            {
+                if (Table[k] == color_ID)
+                {
+                    k++;
+                    if (k > round ) 
+                    {
+                        k = 0;
+                       round++;
+                        MessageBox.Show("Next Round");
+
+                        verify_sequ = false;
+                        DisplaySequence();
+                        verify_sequ = true;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Game Over, Nice Try! Score:" + (round));
+                    Application.Restart();
+                }
+            }
+        }
+
+        /*
+         * ----------------------------------------------------------------------------
+         * Name Fuction : DisplaySequence
+         * Return       : 
+         * Description  : Displays the sequence to imitate
+         * ----------------------------------------------------------------------------
+        */
+
+        private void DisplaySequence() {
+            for (j = 0; j <=round; j++)
+            {
+                switch (Table[j])
+                {
+                    case 1: //RED BUTTON DISPLAY CASE
+                        RebButton.BackColor = Color.FromArgb(255, 0, 0);
+                        this.Refresh();
+                        WaitSecond(FlashTime);
+                        RebButton.BackColor = Color.FromArgb(255, 128, 128); // Light red
+                        this.Refresh();
+                        WaitSecond(FlashTime);
+                        break;
+                    case 2: //YELLOW BUTTON DISPLAY CASE
+                        YellowButton.BackColor = Color.FromArgb(255, 255, 0);
+                        this.Refresh();
+                        WaitSecond(FlashTime);
+                        YellowButton.BackColor = Color.FromArgb(255, 255, 128); // Light yellow
+                        this.Refresh();
+                        WaitSecond(FlashTime);
+                        break;
+                    case 3: //GREEN BUTTON DISPLAY CASE
+                        GreenButton.BackColor = Color.FromArgb(0, 255, 0);
+                        this.Refresh();
+                        WaitSecond(FlashTime);
+                        GreenButton.BackColor = Color.FromArgb(128, 255, 128); // Light green
+                        this.Refresh();
+                        WaitSecond(FlashTime);
+                        break;
+                    case 4: //BLUE BUTTON DISPLAY CASE
+                        BlueButton.BackColor = Color.FromArgb(0, 0, 255);
+                        this.Refresh();
+                        WaitSecond(FlashTime);
+                        BlueButton.BackColor = Color.FromArgb(128, 128, 255); // Light blue
+                        this.Refresh();
+                        WaitSecond(FlashTime);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            MessageBox.Show("Your turn!");
+
+        }
+
+    }   
+}
